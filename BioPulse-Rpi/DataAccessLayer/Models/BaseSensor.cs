af475796ba983ac4dd143674157c7a25469a323c
Mihail <315164@viauc.dk>
2024-11-08 14:30:38 +0100

    seting up database

---
+++ BioPulse-Rpi/DataAccessLayer/Models/BaseSensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public abstract class BaseSensor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsWireless { get; set; }
+        public double LastReading { get; set; }
+        public DateTime LastReadingTime { get; set; }
+
+        public byte Address { get; set; }
+
+
+        public void UpdateTemperature(double newReading)
+        {
+            LastReading = newReading;
+            LastReadingTime = DateTime.Now;
+        }
+    }
+}
