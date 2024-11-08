c59b70106d874aa516ef38fe79eb9a922b1dc837
Mihail <315164@viauc.dk>
2024-11-08 13:21:35 +0100

    Seting up project

---
+++ BioPulse-Rpi/PresentationTier/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PresentationTier.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ BioPulse-Rpi/PresentationTier/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace PresentationTier.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
