946236e4463a2594688134942b8adaa250e384a9
Mihail <315164@viauc.dk>
2024-11-08 21:33:17 +0100

    database created

--- BioPulse-Rpi/DataAccessLayer/AppDbContext.cs
+++ BioPulse-Rpi/DataAccessLayer/AppDbContext.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore.Design;
+
 
 namespace DataAccessLayer
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        // DbSet properties representing tables in the database
+        public DbSet<TemperatureSensor> TemperatureSensors { get; set; }
+       // public DbSet<User> Users { get; set; }
+       // public DbSet<PlantProfile> PlantProfiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace 'hydroponicsystem.db' with your desired database name or path
+            optionsBuilder.UseSqlite("Data Source=hydroponicsystem.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure relationships, constraints, or additional configurations if needed
+        }
     }
 }
---
+++ BioPulse-Rpi/DataAccessLayer/Models/TemperatureSensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using DataAccessLayer.Models.PlantProfile;
+
+namespace DataAccessLayer.Models
+{
+    public class TemperatureSensor : BaseSensor
+    {
+
+
+    }
+}
