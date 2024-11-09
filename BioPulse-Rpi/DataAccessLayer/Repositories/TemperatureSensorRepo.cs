e7b4528b33d2d92da4d33fdd6ee4737acd09cd4d
Mihail <315164@viauc.dk>
2024-11-09 08:31:23 +0100

    Implement repository interface and generic repository

---
+++ BioPulse-Rpi/DataAccessLayer/Repositories/TemperatureSensorRepo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer.Repositories
+{
+    public class TemperatureSensorRepo : GenericRepository<TemperatureSensor>
+    {
+        public TemperatureSensorRepo(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<TemperatureSensor>> GetEnabledSensorsAsync()
+        {
+            return await GetDbSet().Where(sensor => sensor.IsEnabled).ToListAsync();
+        }
+    }
+}
