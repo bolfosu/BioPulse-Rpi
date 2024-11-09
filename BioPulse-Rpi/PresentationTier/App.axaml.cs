9863215d89fc4282f68ddf94b03f6c91e6d93a66
Mihail <315164@viauc.dk>
2024-11-09 09:00:34 +0100

    Registered repositories and set up dependency injection in a custom startup class

--- BioPulse-Rpi/PresentationTier/App.axaml.cs
+++ BioPulse-Rpi/PresentationTier/App.axaml.cs
@@ -1,36 +1,48 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PresentationTier.ViewModels;
 using PresentationTier.Views;
+using System;
 
-namespace PresentationTier;
-
-public partial class App : Application
+namespace PresentationTier
 {
-    public override void Initialize()
+    public partial class App : Application
     {
-        AvaloniaXamlLoader.Load(this);
-    }
+        public static IServiceProvider ServiceProvider { get; private set; }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        public override void Initialize()
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel()
-            };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            singleViewPlatform.MainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
+            AvaloniaXamlLoader.Load(this);
         }
 
-        base.OnFrameworkInitializationCompleted();
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                var host = BuildHost();
+                ServiceProvider = host.Services;
+
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = ServiceProvider.GetRequiredService<MainViewModel>()
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+        private IHost BuildHost()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    var startup = new Startup();
+                    startup.ConfigureServices(services);
+                })
+                .Build();
+        }
     }
 }
---
+++ BioPulse-Rpi/PresentationTier/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer;
+using DataAccessLayer.Repositories;
+
+
+namespace PresentationTier
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register DbContext with SQLite connection
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite("Data Source=hydroponicsystem.db"));
+
+            // Register repositories
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IRepository<TemperatureSensor>, TemperatureSensorRepo>();
+
+            // Register any other services or ViewModels here
+            // services.AddTransient<MainViewModel>();
+        }
+    }
+}
