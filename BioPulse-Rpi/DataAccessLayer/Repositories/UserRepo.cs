babf06f98207433620fb2623aaee62ec719a413c
Mihail <315164@viauc.dk>
2024-11-09 21:10:11 +0100

    Created and tested User profile basic CRUD operations repo

---
+++ BioPulse-Rpi/DataAccessLayer/Repositories/UserRepo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer.Repositories
+{
+    public class UserRepo : GenericRepository<User>
+    {
+        public UserRepo(AppDbContext context) : base(context)
+        {
+        }
+
+        // You can add custom methods related to User if needed
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await GetDbSet().FirstOrDefaultAsync(user => user.Email == email);
+        }
+    }
+}
