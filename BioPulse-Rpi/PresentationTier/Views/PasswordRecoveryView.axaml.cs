5472d2a53339dd23807a78d9ec260d4210e45587
Mihail <315164@viauc.dk>
2024-11-27 12:38:04 +0100

    Created pasword recovery view

---
+++ BioPulse-Rpi/PresentationTier/Views/PasswordRecoveryView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PresentationTier;
+
+public partial class PasswordRecoveryView : UserControl
+{
+    public PasswordRecoveryView()
+    {
+        InitializeComponent();
+    }
+}
